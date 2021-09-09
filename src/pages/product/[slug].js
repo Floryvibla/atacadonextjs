c6ba785f36548f0353a855db8d446daac7b4bc9c
Flory Muenge <flory.contato@gmail.com>
2021-09-09 07:15:52 -0300

    arrumando o erro no build

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import { useRouter } from "next/router";
-
-import LayoutOne from "../../components/layouts/LayoutOne";
-import { capitalizeFirstLetter } from "../../common/utils";
-import { getProductsBySlug } from "../../common/shopUtils";
-import productData from "../../data/product.json";
-import ProductDetailOne from "../../components/productDetail/ProductDetailOne";
-
-export default function pid() {
-  const router = useRouter();
-  const { slug } = router.query;
-  const foundProduct = getProductsBySlug(productData, slug);
-  return (
-    <LayoutOne
-      title={foundProduct && capitalizeFirstLetter(foundProduct.name)}
-      clearSpaceTop
-    >
-      {foundProduct && <ProductDetailOne data={foundProduct} />}
-    </LayoutOne>
-  );
-}
-=======
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
@@ -68,4 +45,3 @@ function productDetail() {
 }
 
 export default React.memo(productDetail);
->>>>>>> dev01
